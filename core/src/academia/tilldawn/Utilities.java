e9435bc6827bec8e8a3f3a4476596e511bfb5639
Andre <andreseguradopinho@gmail.com>
2020-04-09 10:35:35 +0100

    added new Utilities Class referenced its values

---
+++ core/src/academia/tilldawn/Utilities.java
@@ -0,0 +1,15 @@
+package academia.tilldawn;
+
+public class Utilities {
+
+    // Valores constantes e o caralho
+
+    public static final int BACKGROUND_WIDTH = 7280;
+    public static final int BACKGROUND_HEIGHT = 4096;
+
+    public static final int VIEWPORT_WIDTH = 1200;
+    public static final int VIEWPORT_HEIGHT = 660;
+
+    public static final int PICTURE_SIZE = 64;
+
+}
--- desktop/src/academia/tilldawn/desktop/DesktopLauncher.java
+++ desktop/src/academia/tilldawn/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package academia.tilldawn.desktop;
 
+import academia.tilldawn.Utilities;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import academia.tilldawn.Game;
@@ -8,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Game";
-		config.width = 1200;
-		config.height = 800;
+		config.width = Utilities.VIEWPORT_WIDTH;
+		config.height = Utilities.VIEWPORT_HEIGHT;
 		new LwjglApplication(new Game(), config);
 	}
 }
